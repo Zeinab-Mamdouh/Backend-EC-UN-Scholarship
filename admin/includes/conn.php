34c3b07ab3e92bafc782b57388e07a9a591bea1e
Zeinab Mamdouh <105918174+Zeinab-Mamdouh@users.noreply.github.com>
2023-10-06 11:39:29 +0300

    Update

--- admin/includes/conn.php
+++ admin/includes/conn.php
@@ -1,15 +1,1 @@
-<?php
-$servername = "localhost";
-$username = "root";
-$password = "";
 
-try{
-    $options=array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8');
-    $conn = new PDO("mysql:host=$servername;port=3307;dbname=cars", $username, $password, $options);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    //echo "Connected successfully";
-}catch(PDOException $e){
-    echo "Connection failed: " . $e->getMessage();
-}
-
-?>
