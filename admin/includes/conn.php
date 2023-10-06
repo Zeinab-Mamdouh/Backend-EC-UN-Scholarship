2b7b475fae1789ef28946e0f765bbbc99f9a7358
Zeinab Mamdouh <105918174+Zeinab-Mamdouh@users.noreply.github.com>
2023-10-06 04:07:28 +0300

    Updates

--- admin/includes/conn.php
+++ admin/includes/conn.php
@@ -1,25 +1,11 @@
 <?php
-$servername = "sql201.infinityfree.com";
-$username = "if0_35174096";
-$password = "eErtQwwDKwT";
+$servername = "localhost";
+$username = "root";
+$password = "";
 
 try{
     $options=array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8');
-    $conn = new PDO("mysql:host=$servername;port=80;dbname=if0_35174096_cars", $username, $password, $options);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    //echo "Connected successfully";
-}catch(PDOException $e){
-    echo "Connection failed: " . $e->getMessage();
-}
-
-?><?php
-$servername = "sql201.infinityfree.com";
-$username = "if0_35174096";
-$password = "eErtQwwDKwT";
-
-try{
-    $options=array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8');
-    $conn = new PDO("mysql:host=$servername;port=80;dbname=if0_35174096_cars", $username, $password, $options);
+    $conn = new PDO("mysql:host=$servername;port=3307;dbname=cars", $username, $password, $options);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     //echo "Connected successfully";
 }catch(PDOException $e){
